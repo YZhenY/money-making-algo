4d619ed7d8591f32ce6b95e94c22fa4aa4d3b20d
YZhenY <fuz3zy@gmail.com>
2017-12-07 16:15:28 +0000

    created bitcoinity dynamic chart scraper

---
+++ bitcoinity.py
@@ -0,0 +1,34 @@
+## method for scraping bitcoinity
+import requests
+class BitcoinityScraper():
+	"""	Class for scraping Bitcoinity. Use its methods to draw data || Rihgt now just for public methods"""
+	url = "https://data.bitcoinity.org/chart_data"
+	
+	data_type = "bidask_sum"
+	currency = "USD"
+	exchange = "bitfinex"
+	compare = "no"
+	bidask_percent = "1"
+	bidask_unit = "curr"
+	function = "none"
+	timespan = "24h"
+	groups_count = 10
+	resolution = "minute"
+	chart_type = "multi"
+	smoothing = "linear"
+	scale_type = "lin"
+
+	def __init__(self):
+		super(BitcoinityScraper, self).__init__()
+		
+
+	def get_params(self):
+		#returns chart params in a dictionary format
+		return {"data_type":self.data_type,"currency": self.currency,"exchange":self.exchange,
+		"compare":self.compare,"bidask_percent":self.bidask_percent,"bidask_unit":self.bidask_unit,"function":self.function,"timespan":self.timespan,"groups_count":self.groups_count,"resolution":self.resolution,"chart_type":self.chart_type,"smoothing":self.smoothing,"scale_type":self.scale_type}
+
+	def get_chart_data(self):
+		#returns chart data fetched through a post request
+		response = requests.post(self.url, data=self.get_params())
+		return response.content
+		
